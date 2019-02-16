8b4518ee4942df80c32b9e0857997d572ae10cc5
Johnny Ray Alt <johnnyrayalt@gmail.com>
2019-02-15 20:03:05 -0800

    outputs two numbers to command line and decides which is larger

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,53 @@
 #include <random>
 #include <array>
 #include <functional>
+#include <algorithm>
+#include <iterator>
 
+struct player_score
+{
+    int score;
+};
 
 int main()
 {
+    player_score player_one;
+    player_score player_two;
     std::random_device rng;
     std::mt19937 gen(rng());
-    std::uniform_int_distribution<int> dist(1,6);
-    
-    std::array<int, 2> arr;
+    std::uniform_int_distribution<int> dist(1, 6);
 
-    for (auto& val : arr)
+    std::array<int, 2> scores;
+
+    int n;
+    for (n = 0; n < 2; n++)
     {
-        val = dist(gen);
-        std::cout << val << ' ';
+        int i = dist(gen);
+        scores[n] = i;
     };
-    
-    std::cout << '\n' << std::endl;
+
+    player_one.score = scores[0];
+    player_two.score = scores[1];
+
+    std::cout << "player one rolled a " << player_one.score << std::endl;
+    std::cout << "player two rolled a " << player_two.score << std::endl;
+
+    if (player_one.score == player_two.score)
+    {
+        std::cout << "draw!" << std::endl;
+    }
+    else if (player_one.score > player_two.score)
+    {
+        std::cout << "player one wins!" << std::endl;
+    }
+    else
+    {
+        std::cout << "player two wins!" << std::endl;
+    };
+
+    std::cout
+        << '\n'
+        << std::endl;
 
     return 0;
 }
